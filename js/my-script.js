c5fae8140912f8c63fa034bc2db17f66d59be2a6
MHD22 <mhd.sad22@gmail.com>
2020-09-23 16:02:02 +0300

    add loops to do some extra validations for user input

--- js/my-script.js
+++ js/my-script.js
@@ -1,12 +1,20 @@
 
 function printName() { //first function for name
     var name = prompt("Enter your name, please:");
+    while(name == ''){
+        alert("you should enter a name..");
+        name = prompt("Enter your name, please:");
+    }
     elname = document.getElementById("name");
     elname.textContent="Welcome "+ name;
 }
 
 function printTime(){ //second function for printing the time...
     var type= prompt("which type you prefer to show the time:\n if 24H enter (24)\n Or (12) :");
+    while(Number(type) !== 12 && Number(type) !== 24){
+        alert("please enter either '12' OR '24' ");
+        type= prompt("which type you prefer to show the time:\n if 24H enter (24)\n Or (12) :");
+    }
     var time = new Date();
     var hour = time.getHours();
     var minutes = time.getMinutes();
@@ -29,8 +37,6 @@ function printTime(){ //second function for printing the time...
 
     }
     eltime.textContent=result;
-
-
 }
 
 function darkMode(){
@@ -47,5 +53,21 @@ printName();
 printTime();
 darkMode();
 
-
+const addItems=(index)=>{ // index to determine to which list will add ?
+  const items =prompt("enter how many items you want to add.."); 
+  while(! (Number(items)>0) ){
+      alert("you should enter a number..");
+      items =prompt("enter how many items you want to add..");
+  }
+  let adds=''; // for concatenate the tags inside it.
+  let text=''; // for display the text entered by user to each item added ;
+  for (let i = 0; i < items; i++) {
+    text=prompt("enter the tixt of item number "+(i+1)+":");
+    text = text === ''? `item ${i+1} added.` : text; // for check if the user doesn't add text, it will take default value instead.
+    adds+=`<input type="checkbox" name="todo"><label> ${text}</label><br>`;
+      
+  }
+  const el=  document.getElementsByClassName('input-container')[index];
+  el.innerHTML+=adds;
+}
 
