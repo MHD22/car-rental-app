218d7de61cdadff961390a1f39747469e968db88
MHD22 <mhd.sad22@gmail.com>
2020-09-23 11:21:16 +0300

    convert the code into functions.. to be easier and readable

--- js/my-script.js
+++ js/my-script.js
@@ -1,46 +1,51 @@
-//input:
 
-var name = prompt("Enter your name, please:");
-var type= prompt("which type you prefer to show the time:\n if 24H enter (24)\n Or (12) :");
-var dark = prompt("Do you want to change the site to dark mode ?\n (y/n):");
-var d = dark.toLowerCase() === 'y'? true:false;
-var time = new Date();
-var hour = time.getHours();
-var minutes = time.getMinutes();
-var post;
-var result;
-var eltime=document.getElementById('time');
+function printName() { //first function for name
+    var name = prompt("Enter your name, please:");
+    elname = document.getElementById("name");
+    elname.textContent="Welcome "+ name;
+}
 
-//processing:
-if( parseInt(type) === 12){
-    if(hour > 12){
-        hour -=12;
-        post=' PM';
-        result=hour +" : "+ minutes +post;
-        eltime.textContent=result;
+function printTime(){ //second function for printing the time...
+    var type= prompt("which type you prefer to show the time:\n if 24H enter (24)\n Or (12) :");
+    var time = new Date();
+    var hour = time.getHours();
+    var minutes = time.getMinutes();
+    var eltime=document.getElementById('time');
+    var post='';
+    var result='';
+    if( parseInt(type) === 12){
+        if(hour > 12){
+            hour -=12;
+            post=' PM';
+            result=hour +" : "+ minutes +post;
+        }
+        else{
+            post=' AM';
+            result=hour +" : "+ minutes +post;
+        }
     }
     else{
-        post=' AM';
-        result=hour +" : "+ minutes +post;
-        eltime.textContent=result;
+        result=hour +" : "+ minutes;
+
     }
-}
-else{
-    result=hour +" : "+ minutes;
     eltime.textContent=result;
+
+
 }
-if(d){
-    document.getElementsByTagName('body')[0].classList.add('dark');
+
+function darkMode(){
+    var dark = prompt("Do you want to change the site to dark mode ?\n (y/n):");
+    var d = dark.toLowerCase() === 'y'? true:false;
+    if(d){
+        document.getElementsByTagName('body')[0].classList.add('dark');
+    }
+
 }
 
 
+printName();
+printTime();
+darkMode();
 
 
 
-//output:
-console.log(hour+ ' : ' + minutes);
-
-
-
-elname = document.getElementById("name");
-elname.textContent="Welcome "+ name;
